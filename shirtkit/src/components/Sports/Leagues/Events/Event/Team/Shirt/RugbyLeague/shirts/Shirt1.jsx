fac4aab632fcf5438c24ef7615ada5fac5c6f7bf
silver <silverthe165@gmail.com>
2023-05-19 11:10:27 +0300

    "aaaa"

--- shirtkit/src/components/Sports/Leagues/Events/Event/Team/Shirt/RugbyLeague/shirts/Shirt1.jsx
+++ shirtkit/src/components/Sports/Leagues/Events/Event/Team/Shirt/RugbyLeague/shirts/Shirt1.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Shirt1() {
   return (
-    <div>Shirt1</div>
+    <div></div>
   )
 }
--- shirtkit/src/components/Sports/Leagues/Events/Event/Team/Shirt/RugbyLeague/shirts/Shirt4.jsx
+++ shirtkit/src/components/Sports/Leagues/Events/Event/Team/Shirt/RugbyLeague/shirts/Shirt4.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Shirt4() {
   return (
-    <div>Shirt4</div>
+    <div></div>
   )
 }
